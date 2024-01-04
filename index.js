9ccf00782f60715a40f42210e82530a89bbed4e6
Tafsirul islam Rohan <mdrohan8432@gmail.com>
2024-01-04 12:28:09 +0600

    included create operation

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.DB_USER);
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.fy83a0s.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const carBrandCollection = client.db('carCollectionDB').collection('carBrand');
+    
+    app.get('/car', async(req, res)=>{
+      const cursor = carBrandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/car', async(req, res)=>{
+      const AddNewCar = req.body;
+      const result = await carBrandCollection.insertOne(AddNewCar)
+      res.send(result);
+      // console.log(AddNewCar);  
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('car server is running');
+  })
+  
+  app.listen(port, () => {
+    console.log(`car server is running on this': ${port}`)
+  })
